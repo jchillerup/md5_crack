48c96a47d7e078c5c4262aa8d9cae12b2af07691
Jens Christian Hillerup <jch@symphogen.com>
2013-05-06 15:44:37 +0200

    more work on the attack

--- md5_attack/main.c
+++ md5_attack/main.c
@@ -8,15 +8,34 @@
 /* The main attack. Takes a hash value a,b,c,d and tries to determine a preimage. */
 int  attack(uint32_t a, uint32_t b, uint32_t c, uint32_t d) {
 	uint32_t m0, m1, m2;
+	uint32_t *ap, *bp, *cp, *dp;
+	uint32_t *m;
+
 	uint8_t b1, b2, b3, b4;
+	int i;
 	
+	ap = &a;
+	bp = &b;
+	cp = &c;
+	dp = &d;
+
+	/* Initialize the message data structure. */
+	m = (uint32_t *) calloc(16, sizeof(uint32_t));
+
 	/* Fix some m1, m2 */
 	m1 = 0x00000000;
 	m2 = 0x00000000;
 
+	/* Subtract the h0..3 */
+
+
 	/* Calculate backwards and store the result */
+	for (i = 63; i > 48; i--) {
+		md5_round_backwards(&ap, &bp, &cp, &dp, m, i);
+	}
 
-
+	/* At this point, we're looking for a value that after round 48 
+	 * has state *ap *bp *cp *dp. */
 
 	/* Iterate over all m0s */
 	for (b1 = 0x20; b1 < 0x7f; b1++)
@@ -27,7 +46,7 @@ int  attack(uint32_t a, uint32_t b, uint32_t c, uint32_t d) {
 		m0 = b1 << 24 | b2 << 16 | b3 << 8 | b4;
 		m0 = m0;
 
-		/* Calculate forwards until after round 48 */
+		/* Calculate forward until after round 48 */
 	}
 
 	return FALSE;
--- md5_attack/md5.h
+++ md5_attack/md5.h
@@ -8,4 +8,5 @@ struct md5_state {
 };
 
 void md5_round(uint32_t* a, uint32_t* b, uint32_t* c, uint32_t* d, char* m, int r);
+void md5_round_backwards(uint32_t** a, uint32_t** b, uint32_t** c, uint32_t** d, uint32_t* m, int r);
 char * md5(char * input);
