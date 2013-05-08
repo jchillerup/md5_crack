c341a00223831c509c9e332ee707b88d52907ba1
Jens Christian Hillerup <jch@symphogen.com>
2013-05-08 17:25:29 +0200

    put up a benchmark

--- md5_attack/main.c
+++ md5_attack/main.c
@@ -87,6 +87,50 @@ int  attack(uint32_t a, uint32_t b, uint32_t c, uint32_t d, int length) {
 	return FALSE;
 }
 
+int  naive_search(uint32_t a, uint32_t b, uint32_t c, uint32_t d) {
+	uint8_t b1, b2, b3, b4;
+	char* m;
+
+	m = (char*) calloc(5, sizeof(char));
+
+	for (b1 = 0x20; b1 < 0x7f; b1++) {
+		float percentage_done = 100* ((float) b1-0x20) / 0x7f;
+		printf("Percentage through the whole space: %.2f\r", percentage_done );
+
+		for (b2 = 0x20; b2 < 0x7f; b2++) {
+
+			for (b3 = 0x20; b3 < 0x7f; b3++) {
+
+				for (b4 = 0x20; b4 < 0x7f; b4++) {
+					md5_state forward_result;
+
+					/* Establish current m0 */
+					*((uint32_t*) m) = b4 << 24 | b3 << 16 | b2 << 8 | b1;
+					
+					/* Calculate forward until after round 48 */
+					forward_result = md5(m);
+
+					if (*(forward_result.a) == a &&
+						*(forward_result.b) == b &&
+						*(forward_result.c) == c &&
+						*(forward_result.d) == d) {
+							free(m);
+							return TRUE;
+					}
+
+					/* Remember to free */
+					free(forward_result.a);
+					free(forward_result.b);
+					free(forward_result.c);
+					free(forward_result.d);
+				}
+			}
+		}
+	}
+
+	return FALSE;
+}
+
 int main(int argc, char ** argv) {
 	md5_state test_target;
 	clock_t cl;
@@ -99,6 +143,7 @@ int main(int argc, char ** argv) {
 	
 	// TODO: Make the output of the initial MD5 call act as 
 	//       the input to the attack.
+	printf("Performing attack...\n");
 	cl = clock();
 	ret = attack(*test_target.a, *test_target.b,  *test_target.c, *test_target.d, 4);
 	cl = clock() - cl;
@@ -108,8 +153,26 @@ int main(int argc, char ** argv) {
 	} else {
 		printf("\nPreimage not ");
 	}
-	printf("after %f seconds.", ((float)cl)/CLOCKS_PER_SEC);
+	printf("after %f seconds.\n", ((float)cl)/CLOCKS_PER_SEC);
+
+
+	printf("\nNaively searching...\n");
+	cl = clock();
+	ret = naive_search(*test_target.a, *test_target.b,  *test_target.c, *test_target.d);
+	cl = clock() - cl;
+	
+	if (ret == TRUE) {
+		printf("\nPreimage found ");
+	} else {
+		printf("\nPreimage not ");
+	}
+	printf("after %f seconds.\n", ((float)cl)/CLOCKS_PER_SEC);
+
+
+
+#ifdef WINDOWS
 	scanf("%d", &input);
+#endif
 
 	return(EXIT_SUCCESS);
 }
