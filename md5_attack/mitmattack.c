6df7829e2ea37d8b816b8438edcb3e44706e0578
Jens Christian Hillerup <jch@symphogen.com>
2013-05-23 13:37:13 +0200

    adhering to c89 and vs2010

--- md5_attack/mitmattack.c
+++ md5_attack/mitmattack.c
@@ -4,34 +4,40 @@
 #include "md5.h"
 
 int  mitm_attack(uint32_t a, uint32_t b, uint32_t c, uint32_t d, int length) {
-	
-	uint32_t* m = (uint32_t*) calloc(16, sizeof(uint32_t));
+	uint32_t *m;
 	uint32_t *pa, *pb, *pc, *pd;
+	uint8_t ba, bb, bc, bd;
+
+	int fsize, bsize, i;
 	
-	int fsize = (int) pow(BYTES_END - BYTES_BEGIN + 1, 4);
-	int bsize = (int) pow(BYTES_END - BYTES_BEGIN + 1, 1);
+	/* These structures contain the forward and backward strands of MD5 calculation */
+	md5_state *forward_chain, *backward_chain, *fptr, *bptr;
 	
+	fsize = (int) pow(BYTES_END - BYTES_BEGIN + 1, 4);
+	bsize = (int) pow(BYTES_END - BYTES_BEGIN + 1, 1);
+	
+	/* Allocs, be sure to free these */
+	m = (uint32_t*) calloc(16, sizeof(uint32_t));
+
 	pa = (uint32_t*) malloc(sizeof(uint32_t));
 	pb = (uint32_t*) malloc(sizeof(uint32_t));
 	pc = (uint32_t*) malloc(sizeof(uint32_t));
 	pd = (uint32_t*) malloc(sizeof(uint32_t));
-	
-	/* These structures contain the forward and backward strands of MD5 calculation */
-	md5_state* forward_chain  = (md5_state*) calloc(fsize, sizeof(md5_state));
-	md5_state* backward_chain = (md5_state*) calloc(bsize, sizeof(md5_state));
-	
-	md5_state* fptr = forward_chain;
-	md5_state* bptr = backward_chain;
 
-	/* */
+	forward_chain  = (md5_state*) calloc(fsize, sizeof(md5_state));
+	backward_chain = (md5_state*) calloc(bsize, sizeof(md5_state));
+
+	fptr = forward_chain;
+	bptr = backward_chain;
+
 	m[1] = 0x41414141; 
 	
 	printf("  + Calculating forward chain.\n");
 	/* Make forward chain */
-	for (uint8_t ba = BYTES_BEGIN; ba < BYTES_END; ba++) {
-	for (uint8_t bb = BYTES_BEGIN; bb < BYTES_END; bb++) {
-	for (uint8_t bc = BYTES_BEGIN; bc < BYTES_END; bc++) {
-	for (uint8_t bd = BYTES_BEGIN; bd < BYTES_END; bd++) {
+	for (ba = BYTES_BEGIN; ba < BYTES_END; ba++) {
+	for (bb = BYTES_BEGIN; bb < BYTES_END; bb++) {
+	for (bc = BYTES_BEGIN; bc < BYTES_END; bc++) {
+	for (bd = BYTES_BEGIN; bd < BYTES_END; bd++) {
 		md5_state res;
 		m[0] = bd << 24 | bc << 16 | bb << 8 | ba;
 		
@@ -56,16 +62,16 @@ int  mitm_attack(uint32_t a, uint32_t b, uint32_t c, uint32_t d, int length) {
 	
 	printf("  + Calculating backward chain.\n");
 	
-	for  (uint8_t a = BYTES_BEGIN; a < BYTES_END; a++) {
+	for  (ba = BYTES_BEGIN; a < BYTES_END; a++) {
 		md5_state res;
-		m[2] = 0x00008000 | a;
+		m[2] = 0x00008000 | ba;
 		
 		*pa = a - h0;
 		*pb = b - h1;
 		*pc = c - h2;
 		*pd = d - h3;
 		
-		for (int i = 63; i > 48; i--) {
+		for (i = 63; i > 48; i--) {
 			md5_round_backwards(&pa, &pb, &pc, &pd, m, i);
 		}
 		
